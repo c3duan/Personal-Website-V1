1bb037a26032268e49b19d1d943c802fe7166144
c3duan <c3duan@ucsd.edu>
2018-09-05 16:09:57 +0800

    Fixed the skill section of my portfolio. Replaced button clickings with sliding transitions for my three skill sections (language, tool, and courses).

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,11 @@
 */
 
 (function($) {
-
     // Remove no-js class
     $('html').removeClass('no-js');
+
+   // $('.panel').height = $('#skills').height + $('body').max
+
 
     // Animate to section when nav is clicked
     $('header a').click(function(e) {
@@ -25,16 +27,6 @@
             scrollTop: scrollDistance + 'px'
         }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-    });
-
-    $('a.button').click(function()
-    {
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
-
-        $('html, body').animate({
-            scrollTop: scrollDistance - 100 + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
     });
 
     // Scroll to top
@@ -96,4 +88,183 @@
         });
     });
 
+    if($('body').innerWidth() <= 480) {
+        $('.layer .pull-right').width( $('.panel').innerWidth() - $('.skills-list-item .lang-name').width() -
+                                       parseInt($('.layer .skills-list').css('margin-right')) );
+
+        $('.panel').height( $('.layer .content p').height() +
+                            ($('ul.skills-list li').length + 2) * parseInt($('.skills-list-item').css('line-height')) );
+
+        $('#skills').height( $('.panel').height() + $('.bullets il').height() * 3 );
+    }
+    else {
+        $('.layer .pull-right').width( Math.ceil($('.panel').innerWidth() / 2) -
+            $('.skills-list-item .lang-name').width() -
+            parseInt($('.layer .skills-list').css('margin-right')) -
+            parseInt($('.layer .skills-list').css('margin-left')) );
+    }
+
 })(jQuery);
+
+window.panel = function( container ) {
+
+    // Dispatched when the current layer changes
+    var changed = new panel.Signal();
+
+    // All layers in this instance of panel
+    var layers = Array.prototype.slice.call( container.querySelectorAll( '.layer' ) );
+
+    // Flag if the browser is capable of handling our fancy transition
+    var capable =	'WebkitPerspective' in document.body.style ||
+        'MozPerspective' in document.body.style ||
+        'msPerspective' in document.body.style ||
+        'OPerspective' in document.body.style ||
+        'perspective' in document.body.style;
+
+    if( capable ) {
+        container.classList.add( 'capable' );
+    }
+
+    // Create dimmer elements to fade out preceding slides
+    layers.forEach( function( el, i ) {
+        if( !el.querySelector( '.dimmer' ) ) {
+            var dimmer = document.createElement( 'div' );
+            dimmer.className = 'dimmer';
+            el.appendChild( dimmer );
+        }
+    } );
+
+    /**
+     * Transitions to and shows the target layer.
+     *
+     * @param target index of layer or layer DOM element
+     */
+    function show( target, direction ) {
+
+        // Make sure our listing of available layers is up to date
+        layers = Array.prototype.slice.call( container.querySelectorAll( '.layer' ) );
+
+        // Flag to CSS that we're ready to animate transitions
+        container.classList.add( 'animate' );
+
+        // Flag which direction
+        direction = direction || ( target > getIndex() ? 'right' : 'left' );
+
+        // Accept multiple types of targets
+        if( typeof target === 'string' ) target = parseInt( target );
+        if( typeof target !== 'number' ) target = getIndex( target );
+
+        // Enforce index bounds
+        target = Math.max( Math.min( target, layers.length ), 0 );
+
+        // Only navigate if were able to locate the target
+        if( layers[ target ] && !layers[ target ].classList.contains( 'show' ) ) {
+
+            layers.forEach( function( el, i ) {
+                el.classList.remove( 'left', 'right' );
+                el.classList.add( direction );
+                if( el.classList.contains( 'show' ) ) {
+                    el.classList.remove( 'show' );
+                    el.classList.add( 'hide' );
+                }
+                else {
+                    el.classList.remove( 'hide' );
+                }
+            } );
+
+            layers[ target ].classList.add( 'show' );
+
+            changed.dispatch( layers[target], target );
+
+        }
+
+    }
+
+    /**
+     * Shows the previous layer.
+     */
+    function prev() {
+
+        var index = getIndex() - 1;
+        show( index >= 0 ? index : layers.length + index, 'left' );
+
+    }
+
+    /**
+     * Shows the next layer.
+     */
+    function next() {
+
+        show( ( getIndex() + 1 ) % layers.length, 'right' );
+
+    }
+
+    /**
+     * Retrieves the index of the current slide.
+     *
+     * @param of [optional] layer DOM element which index is
+     * to be returned
+     */
+    function getIndex( of ) {
+
+        var index = 0;
+
+        layers.forEach( function( layer, i ) {
+            if( ( of && of == layer ) || ( !of && layer.classList.contains( 'show' ) ) ) {
+                index = i;
+                return;
+            }
+        } );
+
+        return index;
+
+    }
+
+    /**
+     * Retrieves the total number of layers.
+     */
+    function getTotal() {
+
+        return layers.length;
+
+    }
+
+    // API
+    return {
+
+        show: show,
+        prev: prev,
+        next: next,
+
+        getIndex: getIndex,
+        getTotal: getTotal,
+
+        changed: changed
+
+    };
+
+};
+
+/**
+ * Minimal utility for dispatching signals (events).
+ */
+panel.Signal = function() {
+    this.listeners = [];
+}
+
+panel.Signal.prototype.add = function( callback ) {
+    this.listeners.push( callback );
+}
+
+panel.Signal.prototype.remove = function( callback ) {
+    var i = this.listeners.indexOf( callback );
+
+    if( i >= 0 ) this.listeners.splice( i, 1 );
+}
+
+panel.Signal.prototype.dispatch = function() {
+    var args = Array.prototype.slice.call( arguments );
+    this.listeners.forEach( function( f, i ) {
+        f.apply( null, args );
+    } );
+}
